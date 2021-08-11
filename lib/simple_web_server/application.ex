6bfcbaeb3e4a28bceabc99c367464f70646621de
Daichi Hashimura <dhashi92@gmail.com>
2021-08-11 20:37:54 +0900

    Add supervision initial setup

--- lib/simple_web_server/application.ex
+++ lib/simple_web_server/application.ex
@@ -4,17 +4,18 @@ defmodule SimpleWebServer.Application do
   @moduledoc false
 
   use Application
+  require Logger
 
   @impl true
   def start(_type, _args) do
     children = [
-      # Starts a worker by calling: SimpleWebServer.Worker.start_link(arg)
-      # {SimpleWebServer.Worker, arg}
+      {Plug.Cowboy, scheme: :http, plug: SimpleWebServer, options: [port: 8080]}
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: SimpleWebServer.Supervisor]
+    Logger.info("Starting application...")
     Supervisor.start_link(children, opts)
   end
 end
