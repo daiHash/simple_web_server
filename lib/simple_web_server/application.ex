89d9ac4f635db8d83d536de8d90fed3ebd092c47
Daichi Hashimura <dhashi92@gmail.com>
2021-08-14 13:49:28 +0900

    Setup application supervisor

--- lib/simple_web_server/application.ex
+++ lib/simple_web_server/application.ex
@@ -9,13 +9,25 @@ defmodule SimpleWebServer.Application do
   @impl true
   def start(_type, _args) do
     children = [
-      {Plug.Cowboy, scheme: :http, plug: SimpleWebServer, options: [port: 8080]}
+      {Plug.Cowboy,
+        scheme: :http, plug: SimpleWebServer.Router, options: [dispatch: dispatch(), port: 8080]},
+      {Registry, keys: :duplicate, name: Registry.SimpleWebServer}
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: SimpleWebServer.Supervisor]
-    Logger.info("Starting application...")
+    Logger.info("Starting application in http://localhost:8080")
     Supervisor.start_link(children, opts)
+  end
+
+  defp dispatch do
+    [
+      {:_,
+        [
+          {"/ws/[...]", SimpleWebServer.SocketHandler, []},
+          {:_, Plug.Cowboy.Handler, {SimpleWebServer.Router, []}}
+        ]}
+    ]
   end
 end
